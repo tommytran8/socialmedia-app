eb3add7c8e7bccf776fd1f2d95274a1eeb429d76
tommytran8 <trantommy71@gmail.com>
2021-07-01 03:36:22 -0700

    emoji and upload media function

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage'
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useStorage } from 'react-firebase-hooks/storage';
 
 import googlelogo from './assets/google.png';
+
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
 
 const {
   apiKey,
@@ -32,6 +37,7 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore= firebase.firestore();
+const storage = firebase.storage();
 
 const Filter = require('bad-words');
 
@@ -71,12 +77,20 @@ const SignOut = () => {
 }
 
 const MainPage = () =>{
-  const maxPost = 5; // used in posts, only display 5 post at a time
+  const { uid, photoURL } = auth.currentUser; //user's google uid and profile image
+
+  const maxPost = 10; // used in posts, only display 5 post at a time
   const postsRef = firestore.collection('posts'); // gets collection from database called posts
   const query = postsRef.orderBy('createdAt').limit(25); //orders collection of objects by createdAt key
+  
 
   const [posts] = useCollectionData(query, {idField: 'id'}); //listens on real-time to data with a hook and gets the collection
   const [formValue, setFormValue] = useState(''); //react's state hook that changes its value real-time
+  const [emoji, setEmojiPicker] = useState(false);
+  const [fileButton, setFileButton] = useState(false);
+  const [fileURL, setFileURL] = useState(null);
+  const [fileName, setFileName] = useState(null);
+  const [fileType, setFileType] = useState(null);
   // const dummy = useRef() //react's ref hook, so window will always have refernce in view (to auto-scroll MainPage)
 
   const filter = new Filter(); //filters out bad words
@@ -84,10 +98,14 @@ const MainPage = () =>{
   const sendpost = async(e) => {
     e.preventDefault();
 
-    const { uid, photoURL } = auth.currentUser; //user's google uid and profile image
+    // console.log(fileName, fileType, fileURL);
+
 
     await postsRef.add({ //adds new post to collection on firestone
       text: filter.isProfane(formValue) ? filter.clean(formValue) : formValue,
+      fileURL: fileURL,
+      fileName: fileName,
+      fileType: fileType,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid, //google uid of user that made post
       photoURL,
@@ -96,18 +114,126 @@ const MainPage = () =>{
       usersLiked: []
     })
     setFormValue(''); //resets MainPage input field
+    setFileButton(false);
+    setEmojiPicker(false);
+    setFileURL(null);
+    setFileName(null);
+    setFileType(null);
+    document.getElementById("img").src = '';
+    document.getElementById("img").height = 0;
+    document.getElementById("link").href = '';
+    document.getElementById("link").innerText = '';
 
     // dummy.current.scrollIntoView({behavior: 'smooth'}); //scroll down
   }
 
+  const addEmoji = (e)=>{
+    setFormValue(`${formValue}${e.native}`);
+  }
+  const toggleEmojiPicker = (e)=>{
+    e.preventDefault();
+    setEmojiPicker(!emoji);
+  }
+
   return (
     <>
-
       {/* MainPage input */}
-      <form onSubmit={sendpost}>
-        <input value={formValue} placeholder={`Whats on your mind, ${auth.currentUser.displayName}?`} onChange={(e) => {setFormValue(e.target.value);}}/> {/* bind hook to form input */}
-        <button type={"submit"} disabled={!formValue}>Post</button>
-      </form>
+      <section>
+        
+        <form onSubmit={sendpost}>
+          {/* user reply */}
+          <button onClick={toggleEmojiPicker}  >:)</button>
+          <input value={formValue} placeholder={`Whats on your mind, ${auth.currentUser.displayName}?`} onChange={(e) => {setFormValue(e.target.value);}} onKeyPress={(e)=>{ if (e.key === "Enter") { e.preventDefault(); document.getElementById("add_reply").click() }}} /> {/* bind hook to form input */}
+          <a id="link" target="_blank"></a>
+          <img id="img" height="0" src=""></img>
+          <button id="add_reply" type={"submit"} disabled={ fileButton || (!formValue && fileURL == null) }>Add Reply</button>
+
+          {/* file upload */}
+          <button disabled={fileButton} onClick={(e)=>{e.preventDefault(); document.getElementById("file").click() }}>Upload Media</button> {/* WORK HERE 06/26/21 : 
+                                                                                                               DONE   1. NEED set it so that Upload Media immediately lets you choose file without using Choose File input. Upload Media should also be a paper clip like modern apps.
+                                                                                                                      2. Also need to fix some permission issues that sometimes causes app to not show images/files saved on firebase storage server
+                                                                                                                      3. MAYBE also delete images off of server when user deletes post
+                                                                                                                      4. MAYBE also have unique names for each image so it doesnt affect images with same file names
+                                                                                                                      5. Work on channels / profiles
+                                                                                                                      6. UI
+                                                                                                                      7. search bar? */}
+          
+          <input id="file" type={"file"} onChange={(e)=>{
+            const file = e.target.files[0]; 
+            if (file) { // checks if user chooses a file
+              const storageRef = storage.ref(`${uid}/${file.name}`);
+              storageRef.getDownloadURL()
+              .then((downloadURL) => {
+                console.log('File available at', downloadURL);
+                setFileButton(false);
+                if (file.type.startsWith("image")) {
+                  const loadedfile = document.getElementById("img");
+                  loadedfile.src = downloadURL;
+                  loadedfile.height = 250;
+                }
+                else {
+                  const loadedfile = document.getElementById("link");
+                  loadedfile.href = downloadURL;
+                  loadedfile.innerText = file.name;
+                }
+                setFileURL(downloadURL);
+                setFileName(file.name);
+                setFileType(file.type);
+              })
+              .catch((error)=>{
+                const upload = storageRef.put(file);
+                setFileButton(true);
+
+                upload.on('state_changed', 
+                  (snapshot) => {
+                    // Observe state change events such as progress, pause, and resume
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    document.getElementById("file_progress").value = progress;
+                    // switch (snapshot.state) {
+                    //   case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    //     console.log('Upload is paused');
+                    //     break;
+                    //   case firebase.storage.TaskState.RUNNING: // or 'running'
+                    //     console.log('Upload is running');
+                    //     break;
+                    // }
+                  }, 
+                  (error) => {
+                    // Handle unsuccessful uploads
+                  }, 
+                  () => {
+                    // Handle successful uploads on complete
+                    upload.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                      console.log('File available at', downloadURL);
+                      setFileButton(false);
+                      if (file.type.startsWith("image")) {
+                        const loadedfile = document.getElementById("img");
+                        loadedfile.src = downloadURL;
+                        loadedfile.height = 250;
+                      }
+                      else {
+                        const loadedfile = document.getElementById("link");
+                        loadedfile.href = downloadURL;
+                        loadedfile.innerText = file.name;
+                      }
+                      setFileURL(downloadURL);
+                      setFileName(file.name);
+                      setFileType(file.type);
+                    })
+                  }
+                );
+              });
+            }
+          }} style={{display: 'none'}}>
+          </input>
+          {fileButton ? <progress id="file_progress" value="0" max= "100" ></progress> : <></>}
+        </form>
+
+        {/* select emoji */}
+        {emoji ? <Picker onSelect={addEmoji} /> : <></>}
+      </section>
+      
       {/* MainPage */}
       <main>
         {posts && posts.reverse().slice(0, maxPost).map(post => <Post key={post.id} post={post}/>)}
@@ -122,7 +248,7 @@ const MainPage = () =>{
 }
 
 const Post = (props) => {
-  const {text, uid, photoURL, displayName, likes, usersLiked} = props.post;
+  const {text, fileURL, fileName, fileType, uid, photoURL, displayName, likes, usersLiked} = props.post;
 
   const postClass = uid === auth.currentUser.uid ? 'posted' : 'received';
   
@@ -173,6 +299,7 @@ const Post = (props) => {
       <p>{displayName}</p>
       <img src={photoURL}/>
       <p>{text}</p>
+      {fileURL ? fileType.startsWith("image") ? <img src={fileURL} height="250"></img> : <a href={fileURL} target="_blank">{fileName}</a> : <></>}
       <button onClick={likePost}>{`likes: ${likes}`}</button>
     </div>
   ) 
